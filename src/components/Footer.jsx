29d921aa45dc010a421035e8db0803ae824e0d8e
arnaudbrice <mephisteur@gmail.com>
2025-07-29 08:55:14 +0200

    fix: remove div from footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <div className="flex justify-center items-center py-4">
-        <p className="text-center text-md sm:text-xl">
-          ©{new Date().getFullYear()} Daily Diary Made with ❤️ by Arnaud
-          Habenicht
-        </p>
-      </div>
+    <div className="flex justify-center items-center py-4">
+      <p className="text-center text-md sm:text-xl">
+        ©{new Date().getFullYear()} Daily Diary Made with ❤️ by Arnaud Habenicht
+      </p>
     </div>
   );
 };
